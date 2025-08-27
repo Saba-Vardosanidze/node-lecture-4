483cc0053a17756d965f827ee348eee7cb3004f2
Saba-Vardosanidze <sabavardo674@gmail.com>
2025-08-27 20:14:10 +0400

    feature

--- server.js
+++ server.js
@@ -1,10 +1,30 @@
-const http = require('http');
+const express = require('express');
+const app = express();
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plan' });
-  res.end('Hello World');
+const PORT = 3000;
+
+// app.get('/', (req, res) => {
+//   res.send('Hello from Express');
+// });
+// ! dinamyc rout
+// app.get('/product/:id', (req, res) => {
+//   console.log(req.params.id);
+//   res.send(`your: ${req.params.id}`);
+// });
+// ! search params
+// app.get('/product/search', (req, res) => {
+//   console.log(req.query);
+//   res.send('query string');
+// });
+// ! json
+// app.get('/', (req, res) => {
+//   res.json([
+//     { message: 'Welcome', author: 'Giorgi' },
+//     { message: 'Welcome', author: 'Giorgi' },
+//   ]);
+// });
+//! middlewear
+app.
+app.listen(PORT, () => {
+  console.log(`server is running on http://localhost:${PORT}`);
 });
-
-const PORT = process.env.PORT || 3000;
-
-server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
