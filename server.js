4d561226059529c0678a4bde8d2fe13458686ef0
Saba-Vardosanidze <sabavardo674@gmail.com>
2025-08-28 16:08:00 +0400

    feature/homework

--- server.js
+++ server.js
@@ -1,74 +1,29 @@
 const express = require('express');
-// გაძლევს რექვესტზე დამატებით ინფორმაციებს
-const morgan = require('morgan');
-// ახსნა 17 ხაზზე
-const cors = require('cors');
 const app = express();
-
 const PORT = 3000;
 
-// jsons გამოართმევს და გაატანს object ს
-app.use(express.json());
-
-app.use(express.static('public'));
-
-app.use(morgan('dev'));
-
-// ფრჩხილებში რასაც დავწერთ მხოლოდ იმას შეუძლია რექვესტის გაგზავნა
-app.use(cors());
-
-app.use((req, res, next) => {
-  console.log(`[custome]: ${req.method}, ${req.originalUrl}`);
-  next();
-});
-
-app.get('/', (req, res) => {
-  res.send('welcome');
-});
-
-// application level middleware
-
-const users = [
-  { id: 1, username: 'saba', role: 'user' },
-  { id: 2, username: 'lekoo', role: 'admin' },
-];
-
-const adminMiddleware = (req, res, next) => {
-  const user = users[1];
-  if (user.role !== 'admin') {
-    return res.status(403).json({ message: 'nouu' });
-  }
-  next();
-};
-
-app.get('/admin', adminMiddleware, (req, res) => {
-  res.send('admin page');
-});
-
-app.get();
-
-app.use((err, req, res, next) => {
-  res.json({
-    message: err,
-  });
-});
-
-//post request
-const books = [
-  { id: 1, name: 'saba', author: 'user' },
-  { id: 2, name: 'lekoo', author: 'admin' },
+const movies = [
+  { id: 1, title: 'Inception', year: 2010 },
+  { id: 2, title: 'The Matrix', year: 1999 },
 ];
 
 app.use(express.json());
 
-app.post('/book', (req, res) => {
-  console.log(req.body);
-  books.push(req.body);
+app.get('/movies', (req, res) => {
+  res.json(movies);
+});
+
+app.post('/movies', (req, res) => {
+  movies.push(req.body);
 
   return res.status(201).json({
-    message: 'book added succsesfull',
+    message: 'movie added',
     data: req.body,
   });
+});
+
+app.use((err, req, res, next) => {
+  res.json({ message: 'Something went wrong!' });
 });
 
 app.listen(PORT, () => {
